308945d5c91be3525d468f0ffcf6ee9e3fe6eff2
Tanvi Kadam <kadamtanvi27@gmail.com>
2026-04-08 03:30:16 +0530

    Deploying Frontend

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -3,6 +3,7 @@ import Chat from "./Chat.jsx";
 import { MyContext } from "./MyContext.jsx";
 import { useContext, useState, useEffect } from "react";
 import { PacmanLoader } from "react-spinners";
+import server from "./environment.js";
 
 function ChatWindow() {
     const {prompt, setPrompt, reply, setReply, currThreadId, setCurrThreadId, prevChats, setPrevChats, setNewChat} = useContext(MyContext);
@@ -24,7 +25,7 @@ function ChatWindow() {
         };
 
         try {
-            const response = await fetch("http://localhost:8080/api/chat", options);
+            const response = await fetch(`${server.prod}/api/chat`, options);
             const res = await response.json();
             console.log(res);
             setReply(res.reply)
---
+++ Frontend/src/environment.js
@@ -0,0 +1,6 @@
+const server = {
+    dev: "http://localhost:8080",
+    prod: "https://thinkera-backend.onrender.com"
+}
+
+export default server;
