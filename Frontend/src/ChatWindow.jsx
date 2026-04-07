2c3978c7427f51c645f845e715fcd6945e741653
Tanvi Kadam <kadamtanvi27@gmail.com>
2026-04-08 02:36:45 +0530

    Ready for Deployment

---
+++ Frontend/src/ChatWindow.jsx
@@ -0,0 +1,96 @@
+import "./ChatWindow.css";
+import Chat from "./Chat.jsx";
+import { MyContext } from "./MyContext.jsx";
+import { useContext, useState, useEffect } from "react";
+import { PacmanLoader } from "react-spinners";
+
+function ChatWindow() {
+    const {prompt, setPrompt, reply, setReply, currThreadId, setCurrThreadId, prevChats, setPrevChats, setNewChat} = useContext(MyContext);
+    const [loading, setLoading] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const getReply = async () => {
+        setLoading(true);
+        setNewChat(false);
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                message: prompt,
+                threadId: currThreadId
+            })
+        };
+
+        try {
+            const response = await fetch("http://localhost:8080/api/chat", options);
+            const res = await response.json();
+            console.log(res);
+            setReply(res.reply)
+        } catch(err) {
+            console.log(err);
+        }
+        setLoading(false);
+    }
+
+    //Append New Chat to prevChats
+    useEffect(() => {
+        if(prompt && reply) {
+            setPrevChats(prevChats => {
+                return [
+                    ...prevChats, 
+                    { role: "user", content: prompt },
+                    { role: "assistant", content: reply }
+                ]
+            })
+        }
+
+        setPrompt("")
+    }, [reply]);
+
+    const handleProfileClick = () => {
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <div className="ChatWindow">
+            <div className="navbar">
+                <span>ThinkEra <i className="fa-solid fa-angle-down"></i></span>
+                <div className="userIconDiv" onClick={handleProfileClick}>
+                    <span className="userIcon"><i className="fa-solid fa-user"></i></span>
+                </div>
+            </div>
+            {
+                isOpen &&
+                <div className="dropDown">
+                    <div className="dropDownItem"><i class="fa-solid fa-cloud-arrow-up"></i>Upgrade plan</div>
+                    <div className="dropDownItem"><i class="fa-solid fa-person-shelter"></i>Personalization</div>
+                    <div className="dropDownItem"><i class="fa-solid fa-gear"></i>Settings</div>
+                    <div className="dropDownItem"><i class="fa-solid fa-circle-question"></i>Help</div>
+                    <div className="dropDownItem"><i class="fa-solid fa-arrow-right-from-bracket"></i>Log out</div>
+                </div>
+            }
+
+            <Chat></Chat>
+
+            <PacmanLoader color="#fff" loading={loading}>
+
+            </PacmanLoader>
+
+            <div className="chatInput">
+                <div className="inputBox">
+                    <input placeholder="Ask anything" value={prompt} onChange={(e) => setPrompt(e.target.value)} onKeyDown={(e) => e.key === 'Enter'? getReply() : ''}/>
+                    <div id="submit" onClick={getReply}>
+                        <i className="fa-brands fa-telegram"></i>
+                    </div>
+                </div>
+                <p className="info">
+                    ThinkEra can make mistakes. Check important info. See Cookie Preferences.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default ChatWindow;
